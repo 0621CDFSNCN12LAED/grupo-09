01a862500a50b5a1bae94ff0ef9dcc978445be26
“NanoMcmlxx” <cerdajh@gmail.com>
2021-09-01 19:49:57 -0300

    arquitectura MVC y estructur  EJS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
   console.log("Servidor corriendo");
