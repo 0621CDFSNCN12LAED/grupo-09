ae06493f78cf66e24378d614f51d552ade4be474
robertolon <roberto_loncan@hotmail.com>
2021-10-19 19:29:28 -0300

    roberto 19/10

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 /** @format */
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const path = require('path');
-const router = require('./routes/main');
-const productRouter = require('./routes/product');
-const methodOverride = require('method-override');
+const path = require("path");
+const router = require("./routes/main");
+const productRouter = require("./routes/product");
+const methodOverride = require("method-override");
+const session = require("express-session");
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret: "secreto!!" }));
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
-	console.log('Servidor corriendo');
+  console.log("Servidor corriendo");
 });
 
-app.use('/', router);
-app.use('/product', productRouter);
+app.use("/", router);
+app.use("/product", productRouter);
--- routes/product.js
+++ routes/product.js
@@ -1,39 +1,41 @@
 /** @format */
 
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const path = require('path');
-const productController = require('../controllers/productControllers');
+const path = require("path");
+const productController = require("../controllers/productControllers");
 
 const storage = multer.diskStorage({
-	destination: path.join(__dirname, '../../public/img/products'),
-	filename: (req, file, cb) => {
-		cb(null, Date.now() + path.extname(file.originalname));
-	},
+  destination: (req, file, cb) => {
+    cb(path.join(__dirname, "../../public/img/products"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.fieldname + path.extname(file.originalname));
+  },
 });
 
 const uploader = multer({
-	storage,
+  storage,
 });
 
 //get todos los productos
-router.get('/', productController.show);
+router.get("/", productController.show);
 
 //get un solo producto
-router.get('/detail/:id', productController.productDetail);
+router.get("/detail/:id", productController.productDetail);
 
 //get pagina de creacion de producto
-router.get('/create', productController.create);
+router.get("/create", productController.create);
 //Post producto creado a base de datos
-router.post('/', uploader.single('imagen'), productController.store);
+router.post("/", uploader.single("product-image"), productController.store);
 
 //get pagina de editacion de producto
-router.get('/edit/:id', productController.update);
+router.get("/edit/:id", productController.update);
 //put ultima version de producto
-router.put('/:id', productController.edit);
+router.put("/:id", productController.edit);
 
 //Dlete un unico producto
-router.delete('/:id', productController.delete);
+router.delete("/:id", productController.delete);
 
 module.exports = router;
