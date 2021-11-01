13a4d63de87d14381ccc44e2a9aea76720113799
MarcossYoung <marcosyoung98@gmail.com>
2021-11-01 18:13:03 -0300

    doing a pull

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usersControllers');
 
+const uploader = require('../middlewares/userMulter');
+
 //get un solo Usuario
 router.get('/perfil/:id', userController.usuarioDetail);
 
 //get pagina de creacion de Usuario
 router.get('/registro', userController.registro);
 //Post Usuario creado a base de datos
-router.post('/', userController.store);
+router.post('/', uploader.single('imagen'), userController.store);
 
 //get pagina de editacion de Usuario
 router.get('/edit/:id', userController.update);
