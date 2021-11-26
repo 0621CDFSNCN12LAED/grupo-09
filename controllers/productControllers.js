f4551218292ef197913a64a38c89139966adcdf7
“NanoMcmlxx” <cerdajh@gmail.com>
2021-11-26 20:54:46 -0300

    merge

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,35 +1,25 @@
 /** @format */
 
+<<<<<<< Updated upstream
 const loginValidation = require('../validations/loginValidation');
 const {validationResult} = require('express-validator');
+=======
+const loginValidation = require("../validations/loginValidation");
+const { validationResult } = require("express-validator");
+>>>>>>> Stashed changes
 
-const productService = require('../services/product-service');
+const productService = require("../services/product-service");
 
-const {Products, categoriaProduct} = require('../database/models');
+const { Products, categoriaProduct } = require("../database/models");
 
 const product = {
-	//mostrar todos los items
-	show: async (req, res) => {
-		const Prods = await Products.findAll({include: 'categoriaProduct'});
-		res.render('productsAll', {productos: Prods});
-	},
+  //mostrar todos los items
+  show: async (req, res) => {
+    const Prods = await Products.findAll({ include: "categoriaProduct" });
+    res.render("productsAll", { productos: Prods });
+  },
 
-	productDetail: async (req, res) => {
-		const productoUnico = await Products.findByPk(req.params.id, {
-			include: 'categoriaProduct',
-		});
-		if (productoUnico) {
-			res.render('productsDetail', {productoUnico});
-		} else {
-			//error
-			res.redirect('/product');
-		}
-	},
-	// productDetail: (req, res) => {
-	//   const productoUnico = productService.productoUnico(req.params.id);
-	//   res.render("productsDetail", { productoUnico });
-	// },
-
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 	//crear un item nuevo y guardarlos
 	create: async (req, res) => {
@@ -71,18 +61,39 @@ const product = {
   //crear un item nuevo y guardarlos
   create: async (req, res) => {
     // if (loginValidation == true) {
+=======
+  productDetail: async (req, res) => {
+>>>>>>> Stashed changes
     const productoUnico = await Products.findByPk(req.params.id, {
       include: "categoriaProduct",
     });
-    res.render("productCreateForm", { productoUnico });
-    // } else {
-    // res.render("login", {
-    //   err: ["Debes Estar Registrado para Crear"],
-    // });
-    // }
-    // },
-    //va por get y trae el formulario en blanco
+    if (productoUnico) {
+      res.render("productsDetail", { productoUnico });
+    } else {
+      //error
+      res.redirect("/product");
+    }
   },
+  // productDetail: (req, res) => {
+  //   const productoUnico = productService.productoUnico(req.params.id);
+  //   res.render("productsDetail", { productoUnico });
+  // },
+
+  //crear un item nuevo y guardarlos
+  create: async (req, res) => {
+    if (loginValidation == true) {
+      const productoUnico = await Products.findByPk(req.params.id, {
+        include: "categoriaProduct",
+      });
+      res.render("productCreateForm", { productoUnico });
+    } else {
+      res.render("login", {
+        err: ["Debes Estar Registrado para Crear"],
+      });
+    }
+  },
+  //va por get y trae el formulario en blanco
+  //   },
   //guardar datos de los items
   store: async (req, res) => {
     // let errors = validationResult(req);
@@ -104,54 +115,53 @@ const product = {
   //     errors: errors.array(),
   //     old: req.body,
   //   });
->>>>>>> Stashed changes
 
-	//productService.productCreate(req.body, req.file);
-	// res.redirect("/product");
-	//   }
-	// },
-	//modificar un item especifico
-	//extraer item especifico y editarlo
-	//va por put y tiene Id, trae el formulario con los datos del producto
+  //productService.productCreate(req.body, req.file);
+  // res.redirect("/product");
+  //   }
+  // },
+  //modificar un item especifico
+  //extraer item especifico y editarlo
+  //va por put y tiene Id, trae el formulario con los datos del producto
 
-	// update: (req, res) => {
-	//   const productoUnico = productService.productoUnico(req.params.id);
-	//   res.render("productEditForm", { productoUnico });
+  // update: (req, res) => {
+  //   const productoUnico = productService.productoUnico(req.params.id);
+  //   res.render("productEditForm", { productoUnico });
 
-	update: async (req, res) => {
-		const productoUnico = await Products.findByPk(req.params.id);
-		res.render('productEditForm', {productoUnico});
-	},
-	edit: async (req, res) => {
-		// if (loginValidation == true) {
-		// productService.edit(req.params.id, req.body, req.file);
-		await Products.update({
-			// user_id:
-			producto: req.body.producto,
-			proveedor: req.body.proveedor,
-			// vendido:
-			precio: req.body.precio,
-			imagen: req.file.filename,
-			// categoriaproducts_id:
-			descripcion: req.body.description,
-		});
-		res.redirect('/product');
-		// } else {
-		//   res.render("Login", {
-		//     err: ["Debes estar logeado para Editar"],
-		//   });
-		// }
-	},
+  update: async (req, res) => {
+    const productoUnico = await Products.findByPk(req.params.id);
+    res.render("productEditForm", { productoUnico });
+  },
+  edit: async (req, res) => {
+    // if (loginValidation == true) {
+    // productService.edit(req.params.id, req.body, req.file);
+    await Products.update({
+      // user_id:
+      producto: req.body.producto,
+      proveedor: req.body.proveedor,
+      // vendido:
+      precio: req.body.precio,
+      imagen: req.file.filename,
+      categoria: req.body.categoria,
+      descripcion: req.body.description,
+    });
+    res.redirect("/product");
+    // } else {
+    //   res.render("Login", {
+    //     err: ["Debes estar logeado para Editar"],
+    //   });
+    // }
+  },
 
-	//borrar un item especifico
-	delete: async (req, res) => {
-		await Products.destroy({
-			where: {
-				id: req.params.id,
-			},
-		});
-		res.redirect('/product');
-	},
+  //borrar un item especifico
+  delete: async (req, res) => {
+    await Products.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.redirect("/product");
+  },
 };
 
 module.exports = product;
